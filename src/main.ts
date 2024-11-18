28fb6cb40f34d9f8886048c93d1a5f99533c9c1f
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2024-11-18 19:23:53 -0300

    starting setup

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
+import App from './App.vue'
+
+import '@unocss/reset/tailwind.css'
+import './styles/main.css'
+import 'uno.css'
+
+import 'vue-fullpage.js/dist/style.css'
+import VueFullPage from 'vue-fullpage.js'
+
+const app = createApp(App)
+const router = createRouter({
+  routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+})
+app.use(router)
+app.use(VueFullPage)
+app.mount('#app')
---
+++ src/pages/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+defineOptions({
+  name: 'IndexPage',
+})
+
+const options = {
+        licenseKey: 'gplv3-license',
+        anchors: ['home', 'projects', 'contributions', 'contact', 'education'],
+        navigation: true,
+        navigationTooltips: ['Home', 'Projects', 'Contributions', 'Contact', 'Education'],
+        credits: { enabled: false },
+}
+</script>
+
+<template>
+  <div>
+    <full-page ref="fullpage" :options="options" id="fullpage">
+      <div class="section">First section ...</div>
+      <div class="section">Second section ...</div>
+      <div class="section">Second section ...</div>
+      <div class="section">Second section ...</div>
+      <div class="section">Second section ...</div>
+    </full-page>
+  </div>
+</template>
