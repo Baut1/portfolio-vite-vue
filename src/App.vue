7a4f0fcdafb6c0cb62a05bcc7029769c43f2a6d7
Juan Bautista Iglesias <88558284+Baut1@users.noreply.github.com>
2025-01-07 01:33:28 +0000

    naive ui theming

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
-import TheHeader from './components/TheHeader.vue';
+import { darkTheme, lightTheme, NConfigProvider } from 'naive-ui'
+import TheHeader from './components/TheHeader.vue'
 
-  const tetikusOptions = {
-    borderWidth: 3,
-    borderColor: 'DarkSlateGray',
-    invertColor: true,
-    hideOnOut: true,
-    lerp: 0.15,
-  }
+const isDark = useDark()
+const naiveTheme = computed(() => (isDark.value ? darkTheme : lightTheme))
+
+const tetikusOptions = {
+  borderWidth: 3,
+  borderColor: 'DarkSlateGray',
+  invertColor: true,
+  hideOnOut: true,
+  lerp: 0.15,
+}
 </script>
 
 <template>
   <main font-sans text="center gray-700 dark:gray-200">
     <TheHeader />
-    <RouterView />
-    <tetikus :options="tetikusOptions"/>
+    <NConfigProvider :theme="naiveTheme">
+      <RouterView />
+    </NConfigProvider>
+    <tetikus :options="tetikusOptions" />
     <!-- <TheFooter /> -->
   </main>
 </template>
